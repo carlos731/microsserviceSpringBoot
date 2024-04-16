dd1f64ae07eb2f06d9d8fb7510cb67fc1c86db6a
Carlos Henrique <chtsca2001@gmail.com>
2024-04-16 15:14:16 -0300

    Create Microservice Accounts, Cards, and Loans

---
+++ microservice/accounts/src/main/java/com/microsservicechtsca/accounts/repository/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.microsservicechtsca.accounts.repository;
+
+import com.microsservicechtsca.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+
+    Optional<Customer> findByEmail(String email);
+
+}
