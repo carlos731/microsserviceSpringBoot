feccb5cf9e6b119377079e13c7bd0815eec90782
Carlos Henrique <chtsca2001@gmail.com>
2024-05-24 17:08:49 -0300

    Aula 147 - Implementing RateLimiter pattern in accounts

--- microservice/accounts/src/main/java/com/microsservicechtsca/accounts/controller/AccountsController.java
+++ microservice/accounts/src/main/java/com/microsservicechtsca/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.microsservicechtsca.accounts.dto.CustomerDto;
 import com.microsservicechtsca.accounts.dto.ErrorResponseDto;
 import com.microsservicechtsca.accounts.dto.ResponseDto;
 import com.microsservicechtsca.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -218,11 +219,18 @@ public class AccountsController {
             )
         }
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME")); // Variáveis de ambiente e de sistemas Ex: "JAVA_HOME".
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @Operation(
